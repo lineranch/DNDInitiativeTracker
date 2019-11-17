781e032aace3b185a995ce27b1c555241644d373
Hunter <hunteralexanderline@gmail.com>
2019-11-17 09:56:16 -0700

    Moved old files to a legacy folder

--- Legacy/DNDInitiativeDriver.py
+++ Legacy/DNDInitiativeDriver.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from DNDInitiativeTracker import Generator
 
 if __name__ == "__main__":
+    print(sys.path)
     sys.path.append("Generator.py")
     app = QApplication(sys.argv)
     ex = Generator.Generator()                #Instead of making an instance of a window (window = QWidget), We can make this object
